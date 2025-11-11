fb41a793e362abd4d7ff75a6538a0148eaf2a623
RatskuII <ratskuii.mc@gmail.com>
2025-11-11 10:55:22 -0400

    fix?!?!?!

--- src/main/java/dev/Fjc/pinataQuestCounter/PinataQuestCounter.java
+++ src/main/java/dev/Fjc/pinataQuestCounter/PinataQuestCounter.java
@@ -11,55 +11,40 @@ import java.io.IOException;
 public final class PinataQuestCounter extends JavaPlugin {
 
     private static PinataQuestCounter plugin;
-    private static ODailyQuests instance;
-
-    private final FileBuilder fileBuilder = new FileBuilder(this);
+    private FileBuilder fileBuilder;
 
     @Override
     public void onEnable() {
-        if (!getServer().getPluginManager().isPluginEnabled("ODailyQuests")) {
-            this.getLogger().warning("ODailyQuests could not be found! That's not good.");
-            run();
-        } else run();
-
-    }
-
-    public void run() {
         plugin = this;
-        instance = ODailyQuests.INSTANCE;
+        saveDefaultConfig();
 
-        try {
-            registerQuestEvent(new CounterClass(this));
-        } catch (IllegalArgumentException | NullPointerException e) {
-            plugin.getLogger().warning("Something horribly wrong has stopped this quest from registering!");
-            plugin.getLogger().warning(e.getLocalizedMessage());
-        } finally {
-            plugin.getLogger().info("Finished with loading the event. Moving on...");
-        }
-
+        fileBuilder = new FileBuilder(this);
         try {
             fileBuilder.build();
             fileBuilder.loadDefaults();
         } catch (IOException e) {
-            plugin.getLogger().warning("Something went horribly wrong while attempting to construct new configs:");
-            plugin.getLogger().warning(e.getLocalizedMessage());
-        } finally {
-            plugin.getLogger().info("Done.");
+            getLogger().severe("Failed to build data/config files!");
+            e.printStackTrace();
         }
+
+        if (getServer().getPluginManager().isPluginEnabled("ODailyQuests")) {
+            ODailyQuests odq = (ODailyQuests) getServer().getPluginManager().getPlugin("ODailyQuests");
+            getLogger().info("[DEBUG] ODailyQuests detected, registering listener...");
+            registerQuestEvent(new CounterClass(this));
+        } else {
+            getLogger().warning("[DEBUG] ODailyQuests not found, quest tracking disabled.");
+        }
+
+        getLogger().info("PinataQuestCounter enabled successfully!");
     }
 
     @Override
     public void onDisable() {
         plugin = null;
-        instance = null;
     }
 
     public static PinataQuestCounter getPlugin() {
         return plugin;
-    }
-
-    public static ODailyQuests getInstance() {
-        return instance;
     }
 
     public FileBuilder getFileBuilder() {
